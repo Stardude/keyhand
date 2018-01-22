f95de1460b25978d1f87307515330ec8177f82c6
Yura Chubei <chubeyyura@gmail.com>
2018-01-22 13:37:15 +0200

    changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var express = require('express');
+var http = require('http');
 
-const app = express();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-});
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello');
+})
