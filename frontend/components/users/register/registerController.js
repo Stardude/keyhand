14c62d156050204dddfb5c9b1403689e60fd74c8
Yurii Chubei <ychubei@future-processing.com>
2018-04-10 22:09:20 +0300

    Read keys from textarea

--- frontend/components/users/register/registerController.js
+++ frontend/components/users/register/registerController.js
@@ -18,8 +18,53 @@
         });
     }
 
-    function RegisterController() {
-        
+    function RegisterController($scope) {
+        $scope.sample = 'london is the capital of great britain';
+        $scope.onKeyUp = onKeyUp;
+        $scope.onKeyDown = onKeyDown;
+
+        var list = [];
+
+        function onKeyUp(e) {
+            !isValidSampleText() && handleSampleTextError();
+            for (var i = list.length - 1; i >= 0; i--) {
+                if (list[i].key === e.key) {
+                    list[i].keyUpTime = e.timeStamp;
+                    break;
+                }
+            }
+            $scope.sample === $scope.sampleText && handleSampleTextFinish();
+        }
+
+        function onKeyDown(e) {
+            if ((e.keyCode > 64 && e.keyCode < 91) || e.keyCode === 32) {
+                list.push({
+                    key: e.key,
+                    keyUpTime: null,
+                    keyDownTime: e.timeStamp,
+                    keyPressTime: null
+                });
+            }
+        }
+
+        function isValidSampleText() {
+            return $scope.sample.substr(0, $scope.sampleText.length) === $scope.sampleText;
+        }
+
+        function handleSampleTextError() {
+            $scope.sampleText = '';
+            list = [];
+        }
+
+        function handleSampleTextFinish() {
+            for (var i = 0; i < list.length; i++) {
+                list[i].keyPressTime =  list[i].keyUpTime -  list[i].keyDownTime;
+                if (i + 1 !== list.length) {
+                    list[i].keyPauseTime = list[i + 1].keyDownTime -  list[i].keyUpTime;
+                }
+            }
+            console.log(list);
+        }
     }
 
 })(window.angular);
